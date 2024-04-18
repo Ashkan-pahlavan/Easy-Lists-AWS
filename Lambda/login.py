d9810c56b48efbd7f3c42e1070267c8bc929ce1e
Ashkan-pahlavan <153180067+Ashkan-pahlavan@users.noreply.github.com>
2024-04-18 14:43:48 +0200

    update in login code

--- Lambda/login.py
+++ Lambda/login.py
@@ -10,33 +10,16 @@ def calculate_md5(string):
     
 
 def lambda_handler(event, context):
-    # return {
-    #     'statusCode': 200,
-    #     'body': json.dumps(event)
-    # }
-    
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('user_todo')  
 
     salt = 'secret_string'
     
-    # Extrahiere Benutzername und Passwort aus dem Pfadparameter (nicht aus dem Anfragekörper)
-    # username = event['body']['UserName']
-    # password = event['body']['Password'] + salt
+
     
     username = json.loads(event['body'])['UserName']
     password = json.loads(event['body'])['Password'] + username + salt
     
-    print(username)
-    # username = event['pathParameters']['UserName']
-    # password = event['body']['Password'] + salt
-    
-    
-
-    # username = 'CCC'
-    # password = '111' + salt
-
-
     try:
       
         response = table.get_item(
@@ -61,15 +44,8 @@ def lambda_handler(event, context):
         # Überprüfe das Passwort
         stored_password_hash = item.get('Password')
         input_password_hash = calculate_md5(password)
-        # input_password_hash2 = calculate_md5(password2)
-
-        # print(stored_password_hash)
-        # print(input_password_hash)
-        # print(input_password_hash2)
 
         if stored_password_hash == input_password_hash:
-            # Passwort ist korrekt
-            # token_for_auth = 'aB3dE7fG2hI9jK4l'
             response = {
                 'statusCode': 200,
                 'headers': headers,
@@ -77,9 +53,8 @@ def lambda_handler(event, context):
             }
             
             return response
-            # {'token': token_for_auth}
         else:
-            # Passwort ist falsch
+
             return {
                 'statusCode': 401,
                 'body': json.dumps({'message': 'Unauthorized'})
